c4cf45e78f7c8d22620e6ff1bc89190f0ad540fb
Pomo-granade <72248168+Pomo-granade@users.noreply.github.com>
2021-10-01 10:33:28 +0530

    Update divisor.py

--- divisor.py
+++ divisor.py
@@ -1,7 +1,8 @@
 n = int(input("How many apples are there with Harry: "))
 mn = int(input("Minimum numbers of students: "))
+mx = int(input("Maximum numbers of students: "))
 if mn > mx:
-    print("This is not a range")
+    print("This is not a range. Minimum is greater than Maximum")
 
 if mn == mx:
     print("This is not a range. Minimum and Maximum are same")
@@ -10,3 +11,5 @@ else:
     for i in range(mn, mx + 1):
         if n % i == 0:
             print(f"{i} is a divisor of {n}")
+        if n % i != 0:
+            print(f"{i} is not a divisor of {n}")
